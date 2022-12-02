4ee06f4c63914e96a0923275c67d43cc59f79a61
Sebastian <sebaszymek@gmail.com>
2022-12-02 23:20:30 +0100

    setting up project

---
+++ src/main/java/com/example/MovieApiApp/MovieApiApp.java
@@ -0,0 +1,13 @@
+package com.example.MovieApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.MovieApiApp.MovieApiApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/MovieApiApp/MovieApiAppTests.java
@@ -0,0 +1,13 @@
+package com.example.czydziala2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
