9feb359076021202d549caa46e390fcc3ed462d9
Sebastian <sebaszymek@gmail.com>
2022-12-03 04:08:28 +0100

    Added MovieRepository interface extending CrudeRepository and MovieDbService

---
+++ src/main/java/com/example/MovieApiApp/repository/MovieRepository.java
@@ -0,0 +1,24 @@
+package com.example.MovieApiApp.repository;
+
+
+import com.example.MovieApiApp.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface MovieRepository extends CrudRepository<MovieEntity, Long> {
+
+    @Override
+    List<MovieEntity> findAll();
+
+    @Override
+    MovieEntity save(MovieEntity movieEntity);
+
+    @Override
+    Optional<MovieEntity> findById(Long id);
+}
---
+++ src/main/java/com/example/MovieApiApp/service/MovieDbService.java
@@ -0,0 +1,19 @@
+package com.example.MovieApiApp.service;
+
+import com.example.MovieApiApp.entity.MovieEntity;
+import com.example.MovieApiApp.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovieDbService {
+
+    private final MovieRepository repository;
+
+    public List<MovieEntity> getAllMovies(){
+        return repository.findAll();
+    }
+}
