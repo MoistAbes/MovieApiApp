69f6a37537331ba677378ff728213cbaaa91b86e
Sebastian <sebaszymek@gmail.com>
2022-12-04 02:25:39 +0100

    Updated MovieController methods and added POST method

--- src/main/java/com/example/MovieApiApp/controller/MovieController.java
+++ src/main/java/com/example/MovieApiApp/controller/MovieController.java
@@ -2,13 +2,15 @@ package com.example.MovieApiApp.controller;
 
 import com.example.MovieApiApp.client.OMDbClient;
 import com.example.MovieApiApp.dto.MovieDto;
+import com.example.MovieApiApp.entity.MovieEntity;
 import com.example.MovieApiApp.mapper.MovieMapper;
 import com.example.MovieApiApp.service.MovieDbService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,23 +18,31 @@ import java.util.List;
 @RestController
 @RequestMapping("v1/movies")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MovieController {
 
     private final OMDbClient omDbClient;
     private final MovieDbService service;
     private final MovieMapper mapper;
 
+    private static Logger logger = LogManager.getLogger(MovieController.class);
+
     @GetMapping
-    public List<MovieDto> getMovies(){
-        return mapper.mapToMovieDtoList(service.getAllMovies());
+    public ResponseEntity<List<MovieDto>> getMovies(){
+        logger.info("USING GET ENDPOINT");
+        return ResponseEntity.ok(mapper.mapToMovieDtoList(service.getAllMovies()));
     }
 
-    @GetMapping(value = "{title}")
-    public MovieDto getMovie(@PathVariable String title){
-        return omDbClient.getMovieByTitle(title);
+    @GetMapping(value = "/{title}")
+    public ResponseEntity<MovieDto> getMovie(@PathVariable String title){
+        logger.info("USING GET /TITLE ENDPOINT");
+        return ResponseEntity.ok(omDbClient.getMovieByTitle(title));
     }
 
-//    public MovieDto addMovie(){
-//       return new MovieDto();
-//    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addMovie(@RequestBody MovieDto movieDto){
+        MovieEntity movieEntity = mapper.mapToMovieEntity(movieDto);
+        service.saveMovie(movieEntity);
+        return ResponseEntity.ok().build();
+    }
 }
